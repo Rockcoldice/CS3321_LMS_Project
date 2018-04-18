89af0d35400db3b0a5753aa67c40168ea55ee1d2
illybarr <33291685+illybarr@users.noreply.github.com>
2018-04-18 16:25:40 -0500

    Delete student_functions.js

--- student_functions.js
+++
@@ -1,8 +0,0 @@
-function blockPassword() {
-  var enteredValue = document.getElementById('inputPassword');
-  if(enteredValue.type === "password"){
-    enteredValue.type = "text";
-  } else {
-    enteredValue.type = "password";
-  }
-}
