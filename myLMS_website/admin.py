56ec41f4dda20371d9b9d62453910bb083368b0b
Andres Abrego <andres.r.abrego@gmail.com>
2018-04-26 12:09:58 -0500

    Fixed abstracted user model in admin

--- myLMS_website/admin.py
+++ myLMS_website/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(myLMS_webiste.models.user)
+admin.site.register(User)
 admin.site.register(Students)
 admin.site.register(Faculty)
 admin.site.register(Courses)
