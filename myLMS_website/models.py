ba9e56578f125e98f4cb02f80c6b253853e66a8c
Andres Abrego <andres.r.abrego@gmail.com>
2018-04-26 12:13:59 -0500

    Disabled __str__ function

--- myLMS_website/models.py
+++ myLMS_website/models.py
@@ -15,8 +15,8 @@ class Students(models.Model):
     student_id = models.IntegerField(null=True)
     gpa = models.DecimalField(decimal_places=2, max_digits=3, null=True)
 
-    def __str__(self):
-        return "%s %s" % (self.firstName, self.lastName)
+    #def __str__(self):
+    #    return "%s %s" % (self.firstName, self.lastName)
 
     class Meta:
         verbose_name_plural = "Students"
